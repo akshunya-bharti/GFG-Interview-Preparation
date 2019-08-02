ffbbbc375feb1516367020666a4a04b97543a4b1
akshunya-bharti <akshu@live.in>
2019-08-02 14:27:47 +0530

    ReverseWords done

---
+++ src/Problems/String/ReverseWords.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace InterviewPreparation.Problems.String
+{
+    class ReverseWords : Problem
+    {
+        // Replace Sample with your class file name
+        public override string TestFileName { get; set; } = $"{typeof(ReverseWords).Name}.txt";
+        // Replace FOLDER NAME with the directory where your file resides
+        public override string TestFilePath { get; set; } = $"..{Separator}..{Separator}Problems{Separator}String{Separator}";
+        public override List<string> TestInputs { get; set; } = new List<string>();
+        public override List<string> TestOutputs { get; set; } = new List<string>();
+
+        public override void Execute()
+        {
+            var filePath = $"{TestFilePath}{TestFileName}";
+            StreamReader file = new StreamReader(filePath);
+
+            int noOfTestCases = Convert.ToInt32(file.ReadLine());
+
+            for (int i = 0; i < noOfTestCases; i++)
+            {
+                var line = file.ReadLine();
+                TestInputs.Add(line);
+
+                // Finally add the result to Test outputs 
+                var result = ReverseWordsWithDot(line);
+                TestOutputs.Add(result);
+            }
+
+            file.Close();
+        }
+
+        private static string ReverseWordsWithDot(string inputStr)
+        {
+            var outputStr = string.Empty;
+            var inputStrArray = inputStr.Split('.');
+
+            for(int i=inputStrArray.Length - 1; i>0; i--)
+            {
+                outputStr += inputStrArray[i] + ".";
+            }
+
+            outputStr += inputStrArray[0];
+
+            return outputStr;
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -16,7 +16,7 @@ namespace InterviewPreparation
             stopWatch.Start();
 
             // Provide the Class Name of the problem you want to execute
-            var problem = new Problems.String.NonRepeatingCharacter();
+            var problem = new Problems.String.ReverseWords();
 
             try
             {
